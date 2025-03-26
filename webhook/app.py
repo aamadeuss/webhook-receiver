0bb9c4c20da0382c2ba0d0a4e116475d5783847b
Ayushmaan Singh <ayushmaan.singh@ibm.com>
2025-03-26 19:36:45 +0530

    Initial commit

---
+++ webhook/app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+import redis
+import json
+
+app = FastAPI("/receiver")
+
+redis_client = redis.Redis(host="localhost", port=6379, db=0)
+
+@app.post("/webhook")
+async def handle_webhook(
+    request: Request
+):
+    payload = await request.json()
+    redis_client.lpush("wehook_queue", json.dumps(payload))
+    return {
+        "status": "Queued"
+    }
