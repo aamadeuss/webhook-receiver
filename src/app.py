fbc363ffc0ebf6d4c78e36146d0839da423bf81f
Ayushmaan Singh <ayushmaan.singh@ibm.com>
2025-04-03 11:48:11 +0530

    yaml config

--- src/app.py
+++ src/app.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI, Request
-import redis
+from fastapi import FastAPI, Request, Header
+from celery import Celery
 import json
+import yaml
 import os
 
 app = FastAPI()
+config = yaml.safe_load(open('src/config/settings.yaml'))
 
-REDIS_URL=os.getenv("REDIS_URL")
-redis_client = redis.Redis.from_url(REDIS_URL, decode_responses=True)
+# REDIS_URL=os.getenv("REDIS_URL")
+celery_app=Celery(
+    "tasks",
+    broker=config['redis']['url']
+)
 
 @app.get('/health')
 async def healthcheck():
@@ -15,10 +20,16 @@ async def healthcheck():
 
 @app.post("/webhook")
 async def handle_webhook(
-    request: Request
+    request: Request,
+    x_hub_signature: str = Header(None)
 ):
     payload = await request.json()
-    redis_client.lpush("webhook_queue", json.dumps(payload))
-    return {
-        "status": "Queued"
-    }
+    payload_dict = json.loads(payload)
+    command = payload_dict.get("comment", {}).get("body")
+    if (payload_dict.get("action") == "created" and
+        payload_dict.get("issue", {}).get("pull_request") and 
+        command in config['commands']):
+        celery_app.send_task("tasks.process_webhook", args=[payload, command])
+        return {
+            "status": "Queued"
+        }
